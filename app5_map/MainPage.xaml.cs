286a4855d4b50bc81fff4a5dc3015b9197406d53
Jacob Norenberg <jacobn@Jacob.attlocal.net>
2020-03-13 12:32:48 -0700

    initial commit

---
+++ app5_map/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+namespace app5_map
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //var map = new Map(MapSpan.FromCenterAndRadius(new Position(36.8961, 10.1865),
+            //    Distance.FromMiles(0.5)));
+            
+        }
+    }
+}
