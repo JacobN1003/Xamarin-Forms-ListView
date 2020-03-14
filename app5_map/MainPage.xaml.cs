f90a61e7e72833df30e1c80fdcc49b5d09a207f5
Jacob Norenberg <jacobn@Jacob.attlocal.net>
2020-03-14 14:35:59 -0700

    added comments and polish

--- app5_map/MainPage.xaml.cs
+++ app5_map/MainPage.xaml.cs
@@ -13,7 +13,7 @@ namespace app5_map
         public MainPage()
         {
             InitializeComponent();
-            JakesPicker.Items.Add("Pizza Port Bressi Ranch");
+            JakesPicker.Items.Add("Pizza Port Bressi Ranch");//Adds Brewery Names to picker list
             JakesPicker.Items.Add("Black Plague Brewing");
             JakesPicker.Items.Add("Beach Grease Beer Company");
             JakesPicker.Items.Add("Belching Beaver");
@@ -21,8 +21,8 @@ namespace app5_map
 
         void JakesPicker_SelectedIndexChanged(System.Object sender, System.EventArgs e)
         {
-            var name = JakesPicker.Items[JakesPicker.SelectedIndex];
-            if (name == "Pizza Port Bressi Ranch")
+            var name = JakesPicker.Items[JakesPicker.SelectedIndex];//allows me to select each item by name
+            if (name == "Pizza Port Bressi Ranch")//takes the user to the specified pin location on the map
                 JakesMap.MoveToRegion(MapSpan.FromCenterAndRadius(
                     new Position(33.128860, -117.250210), Distance.FromMiles(1)));
             if (name == "Black Plague Brewing")
@@ -36,7 +36,7 @@ namespace app5_map
                     new Position(33.202373, -117.240985), Distance.FromMiles(1)));
         }
 
-        void MapTypes(object sender, EventArgs e)
+        void MapTypes(object sender, EventArgs e)//changes the map type via the button clicks
         {
             if ((sender as Button).Text == "Satellite") JakesMap.MapType = MapType.Satellite;
             if ((sender as Button).Text == "Street") JakesMap.MapType = MapType.Street;
